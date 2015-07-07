1f505418f2363883803d283704afa6f082445f18
Rohit Sharma <skyrohithigh@gmail.com>
2015-07-07 12:38:03 +0530

    Added Session Management Properly

--- src/java/filter/SessionFilter.java
+++ src/java/filter/SessionFilter.java
@@ -22,11 +22,11 @@ import javax.servlet.http.HttpSession;
  * @author rohit
  */
 public class SessionFilter implements Filter {
-
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
-
+    
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
@@ -34,13 +34,16 @@ public class SessionFilter implements Filter {
         HttpSession session = req.getSession(false);
         if (session == null) {
             res.sendRedirect("../adminlogin.jsp");
+        } else if (session.getAttribute("username") == null) {
+            res.sendRedirect("../adminlogin.jsp");
         } else {
+            
             chain.doFilter(request, response);
         }
     }
-
+    
     @Override
     public void destroy() {
     }
-
+    
 }
